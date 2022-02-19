2e1e88c991752911790f72ebbfc1723c475ea4ec
mmilner29 <madisonm4514@gmail.com>
2022-02-19 15:01:30 -0700

    save tasks to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,8 +61,7 @@ var createTaskEl = function(taskDataObj) {
   // increase task counter for next unique id
   taskIdCounter++;
 
-  console.log(taskDataObj);
-  console.log(taskDataObj.status);
+  saveTasks();
 };
 
 var createTaskActions = function(taskId) {
@@ -121,7 +120,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
   };
 
   alert("Task Updated!");
-
+  //save tasks
+  saveTasks();
   // remove data attribute from form
   formEl.removeAttribute("data-task-id");
   // update formEl button to go back to saying "Add Task" instead of "Edit Task"
@@ -168,7 +168,7 @@ var taskStatusChangeHandler = function(event) {
       tasks[i].status = statusValue;
     }
   }
-  console.log(tasks);
+  saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -213,8 +213,12 @@ var deleteTask = function(taskId) {
 
   //reasssign tasks array to be the same as updatedTaskArr
   tasks = updatedTaskArr;
+  saveTasks();
 };
 
+var saveTasks = function() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
 
